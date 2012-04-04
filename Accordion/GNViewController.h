2914c860db47e973f6d6ca9a64f830c7b54b00da
Nicolas Goy <kuon@goyman.com>
2012-04-04 21:54:39 +0200

    Base import

--- Accordion/GNViewController.h
+++ Accordion/GNViewController.h
@@ -1,13 +1,9 @@
-//
-//  GNViewController.h
-//  Accordion
-//
-//  Created by Nicolas Goy on 04.04.12.
-//  Copyright (c) 2012 Goyman.com SA. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-@interface GNViewController : UIViewController
+@interface GNViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+{
+    NSMutableArray *data;
+    NSInteger expandedRowIndex;
+}
 
 @end
--- Accordion/GNViewController.m
+++ Accordion/GNViewController.m
@@ -1,34 +1,109 @@
-//
-//  GNViewController.m
-//  Accordion
-//
-//  Created by Nicolas Goy on 04.04.12.
-//  Copyright (c) 2012 Goyman.com SA. All rights reserved.
-//
 
 #import "GNViewController.h"
 
-@interface GNViewController ()
-
-@end
 
 @implementation GNViewController
 
-- (void)viewDidLoad
+- (void)awakeFromNib
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+    expandedRowIndex = -1;
+    data = [NSMutableArray new];
+    for (int i = 0; i < 1000; i++)
+    {
+        [data addObject:[NSString stringWithFormat:@"Data cell %d", i]];
+    }
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [data count] + (expandedRowIndex != -1 ? 1 : 0);
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSInteger row = [indexPath row];
+    NSInteger dataIndex = [self dataIndexForRowIndex:row];
+    NSString *dataObject = [data objectAtIndex:dataIndex];
+    
+    BOOL expandedCell = expandedRowIndex != -1 && expandedRowIndex + 1 == row;
+    
+    if (!expandedCell)
+    {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"data"];
+        if (!cell)
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"data"];
+        cell.textLabel.text = dataObject;
+        return cell;
+    }
+    else
+    {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"expanded"];
+        if (!cell)
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"expanded"];
+        cell.textLabel.text = dataObject;
+        cell.detailTextLabel.text = [NSString stringWithFormat:@"Details for cell '%@'", dataObject];
+        return cell;
+    }
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSInteger row = [indexPath row];
+    BOOL preventReopen = NO;
+    
+    if (row == expandedRowIndex + 1 && expandedRowIndex != -1)
+        return nil;
+    
+    [tableView beginUpdates];
+    
+    if (expandedRowIndex != -1)
+    {
+        NSInteger rowToRemove = expandedRowIndex + 1;
+        
+        preventReopen = row == expandedRowIndex;
+        if (row > expandedRowIndex)
+            row--;
+        expandedRowIndex = -1;
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:rowToRemove inSection:0]] withRowAnimation:UITableViewRowAnimationTop];
+    }
+    NSInteger rowToAdd = -1;
+    if (!preventReopen)
+    {
+        rowToAdd = row + 1;
+        expandedRowIndex = row;
+        [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:rowToAdd inSection:0]] withRowAnimation:UITableViewRowAnimationTop];
+        
+    }
+    [tableView endUpdates];
+    
+    return nil;
+}
+
+- (CGFloat)tableView:(UITableView *)aTableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSInteger row = [indexPath row];
+    if (expandedRowIndex != -1 && row == expandedRowIndex + 1)
+        return 100;
+    return 40;
+}
+
+- (NSInteger)dataIndexForRowIndex:(NSInteger)row
+{
+    if (expandedRowIndex != -1 && expandedRowIndex <= row)
+    {
+        if (expandedRowIndex == row)
+            return row;
+        else
+            return row - 1;
+    }
+    else
+        return row;
 }
 
 @end
